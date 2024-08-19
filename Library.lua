b9e6776d96ef70384649e31669043953fd24c0f5
Aleksandar <aleksandar.exunys@gmail.com>
2024-08-19 19:48:26 +0200

    Update Library.lua

--- Library.lua
+++ Library.lua
@@ -21,7 +21,7 @@ local Services = {
 	ReplicatedFirst = GetService("ReplicatedFirst"),
 	TeleportService = GetService("TeleportService"),
 	CoreGui = GetService("CoreGui"),
-	VirtualUser = GetService("VirtualUser")
+	VirtualUser = GetService("VirtualUser"),
 	Camera = workspace.CurrentCamera
 }
 
